f0c40565cbe9c2ab68ff37af6a859c9d3b1d77b2
Guilherme José <gui_guifonseca@hotmail.com>
2025-01-10 17:17:17 -0300

    fix: ensure proper linking of User, Member, and Address by setting correct foreign keys during member creation

--- src/repositories/MemberRepository.js
+++ src/repositories/MemberRepository.js
@@ -4,12 +4,13 @@ const User = require('../models/User');
 
 const createMember = async (userData, memberData, addressData, transaction) => {
   const user = await User.create(userData, { transaction });
-  const member = await Member.create({ ...memberData, UserId: user.id }, { transaction });
-  const address = await Address.create({ ...addressData, MemberId: member.id }, { transaction });
+
+  const member = await Member.create({ ...memberData, user_id: user.id }, { transaction });
+
+  const address = await Address.create({ ...addressData, member_id: member.id }, { transaction });
 
   return { user, member, address };
 };
-
 const getAllMembers = async () => {
   return await Member.findAll({
     include: [
