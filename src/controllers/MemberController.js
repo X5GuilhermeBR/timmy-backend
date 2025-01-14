789fe178fcb64aee15c505b06d6adfe37a951df7
Guilherme José <gui_guifonseca@hotmail.com>
2025-01-14 15:50:20 -0300

    fix: adjust member creation endpoint to remove address handling

--- src/controllers/MemberController.js
+++ src/controllers/MemberController.js
@@ -3,21 +3,21 @@ const { createMember, getAllMembers, deactivateMember, updateMember } = require(
 
 class MemberController {
   static async createMember(req, res) {
-    const { user, member, address } = req.body;
-
+    const { user, member } = req.body;
+  
     let transaction;
     try {
       transaction = await sequelize.transaction();
-      const newMemberData = await createMember(user, member, address, transaction);
+      const newMemberData = await createMember(user, member, transaction);
       await transaction.commit();
-
+  
       res.status(201).json({
         message: 'Membro criado com sucesso!',
         data: newMemberData,
       });
     } catch (error) {
       if (transaction) await transaction.rollback();
-
+  
       res.status(500).json({
         message: 'Erro ao criar membro',
         error: error.message,
--- src/repositories/MemberRepository.js
+++ src/repositories/MemberRepository.js
@@ -2,14 +2,12 @@ const Member = require('../models/Member');
 const Address = require('../models/Address');
 const User = require('../models/User');
 
-const createMember = async (userData, memberData, addressData, transaction) => {
+const createMember = async (userData, memberData, transaction) => {
   const user = await User.create(userData, { transaction });
 
   const member = await Member.create({ ...memberData, user_id: user.id }, { transaction });
 
-  const address = await Address.create({ ...addressData, member_id: member.id }, { transaction });
-
-  return { user, member, address };
+  return { user, member };
 };
 
 const getAllMembers = async () => {
