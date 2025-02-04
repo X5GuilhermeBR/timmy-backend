680aef121d25697c8efabe22f2a621f79ec83c2c
Guilherme José <gui_guifonseca@hotmail.com>
2025-02-04 11:27:06 -0300

    feat: sort members by active status first, then alphabetically

--- src/repositories/MemberRepository.js
+++ src/repositories/MemberRepository.js
@@ -55,6 +55,10 @@ const getAllMembers = async () => {
         attributes: [],
       },
     ],
+    order: [
+      ['is_actived', 'DESC'],
+      ['full_name', 'ASC'],
+    ],
     raw: true,
   });
 };
